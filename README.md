913161c82de0c2054f7a754630771a3c83b0363b
MichaelOppong <OppongM@gmail.com>
2024-05-13 14:18:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Button Game
+Try to solve all the challenges below--
+
+### Challenge 1: 
+Fix the game so that the buttons reappear.
+
+### Challenge 2:
+Make a new button that uses .innerHTML to add the following text to the screen: "The solution is 4,2,6,7,3,5,1.""
+
+### Challenge 3:
+Use .style to make the buttons bigger. 
+
+### Challenge 4: 
+Make all the buttons different colors using .style.
+
+# Bonus
+
+### Challenge 5:
+Use .style to change the background color of the screen every time you click a new button. 
+
+### Challenge 6:
+Center the instructions on the page. 
+
+### Challenge 7:
+Make a new button that refreshes all the buttons.
+
+Push your code before doing the last challenge!
+
+### Challenge 8:
+Look at the HTML section of the code and find the secret hidden link. Use JavaScript to make it visible, then follow the link to the secret challenge page!
---
+++ script.js
@@ -0,0 +1,48 @@
+let oneElement = document.querySelector(".one");
+
+oneElement.addEventListener("click", function() {
+  let fiveElement = document.querySelector(".five");
+  fiveElement.style.display = "none";
+});
+
+let twoElement = document.querySelector(".two");
+twoElement.addEventListener("click", function() {
+  let fourElement = document.querySelector(".four");
+  fourElement.style.display = "none";
+});
+
+let threeElement = document.querySelector(".three");
+threeElement.addEventListener("click", function() {
+  let sevenElement = document.querySelector(".seven");
+  sevenElement.style.display = "none";
+});
+
+let fourElement = document.querySelector(".four");
+fourElement.addEventListener("click", function() {
+  let eightElement = document.querySelector(".eight");
+  eightElement.style.display = "none";
+});
+
+let fiveElement = document.querySelector(".five");
+fiveElement.addEventListener("click", function() {
+  let threeElement = document.querySelector(".three");
+  threeElement.style.display = "none";
+});
+
+let sixElement = document.querySelector(".six");
+sixElement.addEventListener("click", function() {
+  let twoElement = document.querySelector(".two");
+  twoElement.style.display = "none";
+});
+
+let sevenElement = document.querySelector(".seven");
+sevenElement.addEventListener("click", function() {
+  let sixElement = document.querySelector(".six");
+  sixElement.style.display = "none";
+});
+
+let eightElement = document.querySelector(".eight");
+eightElement.addEventListener("click", function() {
+  let oneElement = document.querySelector(".one");
+  oneElement.style.display = "none";
+});
